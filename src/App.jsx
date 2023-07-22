39e7be7debd93d09c8445f7058cd3f5df94c8028
Margarita Varetckaia <mvaretckaia@gmail.com>
2023-07-22 14:12:58 +0200

    feat(ui): dark light mode

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Routes, Route } from 'react-router-dom';
 import ImagePage from './ImagePage/ImagePage';
 function App() {
   const [images, setImages] = useState([]);
+  const [mode, setMode] = useState('light');
 
   useEffect(() => {
     API.getImages().then((images) => {
@@ -13,8 +14,16 @@ function App() {
     });
   }, []);
 
+  function toggleMode() {
+    setMode(mode === 'light' ? 'dark' : 'light');
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', mode);
+  }, [mode]);
   return (
     <>
+      <button onClick={toggleMode}>dark</button>
       <Routes>
         <Route path="/" element={<List images={images} />}></Route>
         <Route path="image/:id" element={<ImagePage images={images} />}></Route>
