6c4737b72c4ea1d235da0e974479caeb0ab8c62d
Margarita Varetckaia <mvaretckaia@gmail.com>
2023-07-25 13:39:02 +0200

    feat: add viewport function for responsive styles

--- src/ImagePage/ImageCard.jsx
+++ src/ImagePage/ImageCard.jsx
@@ -1,7 +1,10 @@
 import { useContext } from 'react';
 import { ThemeContext } from '../utils/context';
+import { useViewport } from '../utils/viewport';
+
 export const ImageCard = ({ image, comments, setReadingMode, readingMode }) => {
   const theme = useContext(ThemeContext);
+  const { width } = useViewport();
 
   return (
     <div className=" flex flex-col max-w-full items-center px-4">
@@ -25,7 +28,7 @@ export const ImageCard = ({ image, comments, setReadingMode, readingMode }) => {
                 viewBox="0 0 24 24"
                 stroke-width="1.5"
                 stroke="currentColor"
-                class="w-6 h-6"
+                className="w-6 h-6"
                 alt="comment icon to add comments section"
               >
                 <path
@@ -39,13 +42,13 @@ export const ImageCard = ({ image, comments, setReadingMode, readingMode }) => {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                class="w-6 h-6"
+                className="w-6 h-6"
                 alt="comment icon to add comments section"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M4.848 2.771A49.144 49.144 0 0112 2.25c2.43 0 4.817.178 7.152.52 1.978.292 3.348 2.024 3.348 3.97v6.02c0 1.946-1.37 3.678-3.348 3.97a48.901 48.901 0 01-3.476.383.39.39 0 00-.297.17l-2.755 4.133a.75.75 0 01-1.248 0l-2.755-4.133a.39.39 0 00-.297-.17 48.9 48.9 0 01-3.476-.384c-1.978-.29-3.348-2.024-3.348-3.97V6.741c0-1.946 1.37-3.68 3.348-3.97zM6.75 8.25a.75.75 0 01.75-.75h9a.75.75 0 010 1.5h-9a.75.75 0 01-.75-.75zm.75 2.25a.75.75 0 000 1.5H12a.75.75 0 000-1.5H7.5z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 />
               </svg>
             )}
