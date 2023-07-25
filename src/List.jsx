535105acf69e8722393c990bb5c962f8be4f1455
Margarita Varetckaia <mvaretckaia@gmail.com>
2023-07-25 13:24:33 +0200

    refactor(logos): change link logo to heroicons

--- src/List.jsx
+++ src/List.jsx
@@ -9,7 +9,22 @@ function ListItem({ image }) {
           <p className="text-xs"> id: {image.id}</p>
         </div>
         <div className="flex justify-end">
-          <div className="">🔗</div>
+          <div className="">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
+              />
+            </svg>
+          </div>
         </div>
       </div>
     </Link>
