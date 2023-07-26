9a816c30ab6abc679472351df9d58ba714cb98c1
Margarita Varetckaia <mvaretckaia@gmail.com>
2023-07-26 11:43:28 +0200

    feat(server): add mongoose schema

---
+++ apps/server/src/mongo.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+
+const commentSchema = new mongoose.Schema({
+  _id: Object,
+  text:String,
+  time: String
+})
+
+export const DBComment = mongoose.model(  
+  'DBComment',
+  commentSchema,
+  'comments'
+)
