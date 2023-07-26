4f7d13efe985589661602de5814896dd14bcb6c7
Margarita Varetckaia <mvaretckaia@gmail.com>
2023-07-26 18:34:23 +0200

    style(commentCard): make padding x smaller

--- apps/client/src/ImagePage/Comments.jsx
+++ apps/client/src/ImagePage/Comments.jsx
@@ -1,6 +1,6 @@
 export const CommentCard = ({ comment }) => {
   return (
-    <div className="mx-2 rounded-lg border-[0.5px] flex flex-col items-left my-1 py-3 justify-between text-left px-8 max-w-full  bg-my3 grid-flow-col ">
+    <div className="mx-2 rounded-lg border-[0.5px] flex flex-col items-left my-1 py-3 justify-between text-left px-4 max-w-full  bg-my3 grid-flow-col ">
       <p className="flex text-xs opacity-80">{comment.time}</p>
       <p className="flex break-all">{comment.text}</p>
     </div>
