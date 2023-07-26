cfe66d35f3ed43256d82e560e3f739986d176e3c
Margarita Varetckaia <mvaretckaia@gmail.com>
2023-07-26 12:17:22 +0200

    refactor(client): new comment goes to the db

--- apps/client/src/ImagePage/Comments.jsx
+++ apps/client/src/ImagePage/Comments.jsx
@@ -1,7 +1,4 @@
-import moment from 'moment';
-
 export const CommentCard = ({ text }) => {
-  const time = moment().fromNow();
   return (
     <div className="mx-2 rounded-lg border-[0.5px] flex flex-col items-left my-1 py-3 justify-between text-left px-8 max-w-full  bg-my3 grid-flow-col ">
       <p className="flex text-xs opacity-80">{time}</p>
--- apps/client/src/ImagePage/ImagePage.jsx
+++ apps/client/src/ImagePage/ImagePage.jsx
@@ -2,6 +2,8 @@ import { useEffect, useRef, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { WritingComments, ReadingComments, ImageReadingComments } from './CommentsStates';
 import { ImageCard } from './ImageCard';
+import axios from 'axios'
+import moment from 'moment'
 
 function ImagePage({ images }) {
   const { id } = useParams();
@@ -10,6 +12,7 @@ function ImagePage({ images }) {
   const [readingMode, setReadingMode] = useState(false);
   const [writingMode, setWritingMode] = useState(false);
   const commentsContainerRef = useRef(null);
+  
 
   const image = images.find((item) => item.id == id);
 
@@ -19,11 +22,18 @@ function ImagePage({ images }) {
     }
   }, [comments]);
 
-  function handleSubmitComment() {
-    if (comment.trim() !== '') {
-      setComments([...comments, comment]);
-      setComment('');
-    }
+  async function handleSubmitComment() {
+
+const createdAt = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+await axios. post(`http://localhost:8080/image/${id}`, {
+  text: comment,
+  time: createdAt
+}).then(()=> {
+  setComments('')
+}).catch((e) => {
+  alert(e.response.data.error)
+})
   }
 
   function handleInputComment(e) {
