f5b85c29222bcdd6decc378a8e4a9c501e45cca3
Margarita Varetckaia <mvaretckaia@gmail.com>
2023-07-28 17:34:16 +0200

    refactor(client): change number of files to calculate all api and dicoms

--- apps/client/src/List.jsx
+++ apps/client/src/List.jsx
@@ -35,7 +35,7 @@ function ListItem({ image }) {
 
 function DicomItem({ item }) {
   return (
-<Link className="hover:bg-my2" to={`/image/${item.id}`}>
+    <Link className="hover:bg-my2" to={`/image/${item.id}`}>
       <div className=" text-left px-4 grid border-my2 grid-flow-col border-b-[1px] min-h-12 w-full py-2 mt-2 items-center">
         <div className="flex flex-col  ">
           <h1 className="text-md text"> Example dicom image</h1>
@@ -62,7 +62,7 @@ function DicomItem({ item }) {
         </div>
       </div>
     </Link>
-  )
+  );
 }
 
 function List({ images }) {
@@ -80,9 +80,8 @@ function List({ images }) {
             {dicoms.map((item) => (
               <DicomItem key={item.id} item={item}></DicomItem>
             ))}
-
           </div>
-          <div className="py-8">{images.length} files</div>
+          <div className="py-8">{images.length + dicoms.length} files</div>
         </div>
       )}
     </div>
